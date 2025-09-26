d9994b379d44f3b6f7b433a7f480bb326487bda6
Lukeneph <estiven.ribeiro.s.b@gmail.com>
2025-09-26 19:29:31 +0000

    xadrez iniciante

--- xadrez.c
+++ xadrez.c
@@ -8,14 +8,14 @@ int main() {
 
    printf("Torre\n");
    for(int i = 0; i < Torre; i++) {
-      printf("Direita\n");
+      printf("Direita (%d)\n", i + 1);
    }
    printf(" \n");
 
    printf("Bispo\n");
    int Bispo_matematico = 0;
    while (Bispo_matematico < Bispo) {
-      printf("Diagonal Direita\n");
+      printf("Diagonal Direita (%d)\n", Bispo_matematico + 1);
       Bispo_matematico++;
    } 
    printf(" \n");
@@ -24,7 +24,7 @@ int main() {
    printf("Rainha\n");
    int Rainha_matematico = 0;
    do {
-      printf("Esquerda\n");
+      printf("Esquerda (%d)\n", Rainha_matematico + 1);
       Rainha_matematico++;
    } while (Rainha_matematico < Rainha);
    printf(" \n"); 
